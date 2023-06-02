511d596c15ec5950871551e6ece66ed33a4347d4
1ehun <l3hunbot@gmail.com>
2023-06-02 12:04:12 +0300

    firebase test

--- src/pages/Articles/index.jsx
+++ src/pages/Articles/index.jsx
@@ -1,21 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getDatabase,ref,get } from "firebase/database"
+
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 import './index.css'
 
-const Articles = (props) => {
-    const dispatch = useDispatch() //dispatch нужен для изм состояния
-    const articles = useSelector(state => state.article.articles)
 
-    console.log(articles)
+
+const Articles = (props) => {
+    const [postList,setPostList] = useState(null)
+
+    const dispatch = useDispatch() //dispatch нужен для изм состояния
+    const articlesList = useSelector(state => state.article.articles)
+
+    useEffect(() => {
+        const database = getDatabase();
+        const blogRef = ref(database, "articles");
+        get(blogRef).then((snapshot) => {
+            if (snapshot.exists()) {
+                const blog = snapshot.val();
+                setPostList(Object.values(blog))
+            }
+        })
+    },[])
+
+    console.log(articlesList)
     return(
         <div className="main">
             <div className="main-content">
                 <div className="main-content-block">
                 <h1 className="main-content-header">Добавленные статьи</h1>
              
-                <div className="articles-cards">{articles.map((item) => (
+                <div className="articles-cards">
+                {postList?.map((post) => (
+                    <div>
+                        <p>{post.id}</p>
+                        <p>{post.title}</p>
+                        <p>{post.body}</p>
+                    </div>
+                ))}
+                    {/* {articlesList.map((item) => (
                     <div className="articles-cards-item" >
                         <Link className="article-cards-link" to={`articles/${item.id}`}>
                         <h1 className="article-cards-head">{item.title}</h1>
@@ -23,7 +48,8 @@ const Articles = (props) => {
                         </Link>
                        
                     </div>
-                ))}</div>
+                ))} */}
+                </div>
                 {/* <div className="preview">{article13}</div> */}
                     
 
